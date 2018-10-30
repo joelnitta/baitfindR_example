365d9e2c884d6b1e6a11db2464ccb5168ba08c0c
Joel Nitta <joelnitta@gmail.com>
2018-10-30 00:46:12 +0000

    Copy plan_03 from baitfindR_drake

--- R/main_plan.R
+++ R/main_plan.R
@@ -232,6 +232,180 @@ make_homologs <- drake_plan(
     depends = ortholog_fasta)
 )
 
+# plan_03
+
+# In plan_03, we will account for introns in the candidate bait sequences.
+
+# Setup drake cache -------------------------------------------------------
+
+if (file.exists(".plan_03_cache")) {
+  plan_03_cache <- this_cache(".plan_03_cache")
+} else {
+  plan_03_cache <- new_cache(".plan_03_cache")
+}
+
+# Define basic input values -----------------------------------------------
+
+# Vector of reference genomes to use for making masked blast db
+genomes <- c("arabidopsis", "azolla", "salvinia")
+
+# Ortholog pruning method selected after examining output of Plan 2
+my_prune_method <- "ortho_MI"
+
+# Subfolders for the output of this plan
+subfolders <- c("taxonomy_filtered", "blast_filtered")
+
+# Setup folders ---------------------------------------------------------
+
+plan_03_folders <- drake_plan(
+  top_folder = make_dir(dir_name = "introns", 
+                        outfile = file_out(here::here("introns/.name")))) %>%
+  bind_plans(
+    drake_plan(
+      subfolder = make_dir(dir_name = "introns/subfolder__",
+                           infile = file_in(here::here("introns/.name")),
+                           outfile = file_out(here::here("introns/subfolder__/.name")))
+    )
+  ) %>%
+  evaluate_plan(rules = list(subfolder__ =  subfolders))
+
+# Make intron-masked blast DB ---------------------------------------------
+
+# Generate fasta files of intron-masked genes from reference genomes
+mask_genes_plan <- drake_plan (
+  
+  # Find introns in reference genomes
+  introns = find_bed_regions (
+    gff3_file = get_gff3("genome_list"),
+    genome_file = get_genome("genome_list"),
+    source_select = get_sources("genome_list"),
+    prefix = "genome_list",
+    out_type = "write_all",
+    out_dir = here::here("introns"),
+    output = c(
+      file_out(here::here("introns/genome_list_genes")),
+      file_out(here::here("introns/genome_list_introns")),
+      file_out(here::here("introns/genome_list_exons"))
+    )
+  ),
+  
+  # Mask introns in reference genomes
+  masked_genome = mask_genome(
+    introns_file = file_in(here::here("introns/genome_list_introns")),
+    genome_file = get_genome("genome_list"),
+    masked_genome_file = file_out(here::here("introns/genome_list_masked")),
+    wd = here::here("introns")
+  ),
+  
+  # Extract intron-masked genes from genomes
+  masked_genes = extract_masked_genes(
+    genes_file = file_in(here::here("introns/genome_list_genes")),
+    masked_genome_file = file_in(here::here("introns/genome_list_masked")),
+    masked_genes_file = file_out(here::here("introns/genome_list_masked_genes")),
+    wd = here::here("introns")
+  )
+) %>%
+  evaluate_plan(
+    wildcard = "genome_list",
+    values = genomes)
+
+# Concatenate all the intron-masked genes into a single file
+concatenate_masked_genes_plan <- drake_plan(
+  read = readr::read_file(file_in(here::here("introns/genome_list_masked_genes")))) %>%
+  evaluate_plan(wildcard = "genome_list",
+                values = genomes) %>%
+  bind_plans(gather_plan(., target = "masked_gene_files")) %>%
+  bind_plans(drake_plan(
+    concatenate = baitfindR::cat_files(masked_gene_files, 
+                                       output_file=file_out(here::here("introns/all_masked_genes")))))
+
+# Make blast-db of masked genes
+masked_genes_blast_db_plan <- drake_plan(
+  masked_db = baitfindR::build_blast_db(
+    in_seqs = file_in(here::here("introns/all_masked_genes") ),
+    out_name = here::here("introns/masked_genes"),
+    other_args = "-parse_seqids",
+    db_type = "nucl",
+    outfile = file_out(here::here("introns/masked_genes.nhr"),
+                       here::here("introns/masked_genes.nin"),
+                       here::here("introns/masked_genes.nsq")))
+)
+
+# Mask introns in baits ---------------------------------------------------
+
+mask_baits_plan <- drake_plan (
+  
+  # Filter baits by family
+  # (each alignment must contain at least
+  # one sample per ingroup family)
+  family_filtered_baits = filter_fasta(seq_folder = here::here(paste0(my_prune_method, "/fasta")),
+                                       taxonomy_data = onekp_data,
+                                       filter_col = "family",
+                                       sample_col = "code",
+                                       depends = masked_db),
+  
+  # Write out unaligned, family-filtered baits
+  family_filtered_baits_out = write_fasta_files(
+    fasta_list = family_filtered_baits,
+    out_dir = here::here("introns/taxonomy_filtered")),
+  
+  # Align family-filtered baits in folder
+  aligned_baits = mafft_wrapper(
+    fasta_folder = here::here("introns/taxonomy_filtered"),
+    number_cores = 2,
+    overwrite = TRUE,
+    get_hash = TRUE,
+    depends = family_filtered_baits_out
+  ),
+  
+  # Clean up alignments in folder
+  cleaned_aligned_baits = phyutility_wrapper(
+    fasta_folder = here::here("introns/taxonomy_filtered"),
+    min_col_occup = 0.3,
+    overwrite = TRUE,
+    depends = aligned_baits),
+  
+  # Blast clean, aligned baits against intron-masked genes
+  blast_baits = blastn_list(
+    fasta_folder = here::here("introns/taxonomy_filtered"),
+    fasta_ending = "\\.aln-cln$",
+    database = here::here("introns/masked_genes"),
+    overwrite = TRUE,
+    depends = cleaned_aligned_baits
+  ),
+  
+  # Extract top blast hit for each bait
+  best_hits = extract_blast_hits(
+    blast_db = here::here("introns/masked_genes"),
+    out_dir = here::here("introns/blast_filtered/"),
+    blast_results_folder = here::here("introns/taxonomy_filtered/"),
+    blast_results_ending = "\\.outfmt6$",
+    depends = blast_baits
+  ),
+  
+  # Re-align filtered baits with top blast hits
+  combined_alignments = realign_with_best_hits(
+    blast_filtered_folder = here::here("introns/blast_filtered"),
+    taxonomy_filtered_folder = here::here("introns/taxonomy_filtered"),
+    depends = best_hits
+  ),
+  
+  # Fill-in introns and remove outgroup
+  combined_alignments_filled = fill_introns_loop (
+    alignment_list = combined_alignments,
+    outgroup = "FQGQ"
+  ),
+  
+  # Calculate summary statistics for alignments
+  combined_alignments_data = assemble_bait_data (combined_alignments_filled),
+  
+  # Filter alignments to get final baits
+  final_baits_data = filter_alignments (combined_alignments_data),
+  
+  final_baits = pull(final_baits_data, alignment)
+  
+)
+
 # output report
 # report_plan <- drake_plan(
 #   rmarkdown::render(
